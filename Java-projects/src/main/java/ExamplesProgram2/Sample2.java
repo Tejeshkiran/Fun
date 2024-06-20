1605ee752be82b78765888567ee8f8eb03b5b2f5
Tejeshkiran <tejeshkirant@gmail.com>
2024-06-20 13:15:14 +0530

    this is pst update

--- Java-projects/src/main/java/ExamplesProgram2/Sample2.java
+++ Java-projects/src/main/java/ExamplesProgram2/Sample2.java
@@ -2,10 +2,13 @@ package ExamplesProgram2;
 
 import Examplesprograms.Main2;
 
+import java.sql.SQLOutput;
+
 public class Sample2 {
     public static void main(String[] args) {
         Main2 m = new Main2();
         m.access();
+        System.out.println("New changes");
 
     }
 }
